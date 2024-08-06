bb40306ac9305aa3d62c823deec8f029916fce8a
Daniella Horvath <daniellahorvath92@gmail.com>
2024-08-06 21:51:02 +0100

    Added app.use, app.post, app.put methods

--- src/app.js
+++ src/app.js
@@ -30,9 +30,35 @@ app.get("/musicians/:id", async (req, res) => {
     }else{
         res.status(404).json({ error: "Musicians not found!"})
     }
+});
+
+// GET, POST, PUT, DELETE (Part-3)
+//CREATE -USE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.post("/musicians", async (req, res) => {
+    const newMusicians = await Musician.create(req.body);
+
+    if(!newMusicians){
+        res.status(404).json({ error: "Missing Musician!"});
+        return
+    }
+    res.status(201).json(newMusicians);
+});
+
+// UPDATE - PUT/PATCH
+add.put("/musicians/:id", async (req, res) => {
+    const updateMus = await Musician.findByPk(req.params.id);
+
+    if(!updateMus){
+        res.status(404).json({ error: 'Musician not updated!'})
+        return
+    }
+
+    await Musician.update(req.body);
+    res.status(200).json(updateMus);
 })
-
-
 
 
 
